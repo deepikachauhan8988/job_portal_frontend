6e5b69b4f40df1b40ac6b767bb241b9f563d0c1a
Deepika <deepikachauhan8988@gmail.com>
2025-07-22 12:11:31 +0530

    UI changes

--- src/componets/user/top-navbar/NavBar.js
+++ src/componets/user/top-navbar/NavBar.js
@@ -26,35 +26,64 @@ function NavBar() {
 
   return (
     <>
-      <Navbar expand="lg" className="nav-top-bg" sticky="top">
+      <Navbar expand="lg" className="nav-top-bg main-nav" sticky="top">
         <Container>
-          <Navbar.Brand as={Link} to="/">BrainRock Job Portal</Navbar.Brand>
+          <Navbar.Brand as={Link} to="/">
+            BrainRock Job Portal
+          </Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="me-auto gap-4 justify-content-end flex-grow-1 d-flex">
-
+            <Nav className="me-auto nav-list gap-4 justify-content-end flex-grow-1 d-flex">
               <NavDropdown title="Training" id="training-dropdown">
-                <NavDropdown.Item as={Link} to="/TraningRaact">React JS</NavDropdown.Item>
-                <NavDropdown.Item as={Link} to="/TraningPython">Python</NavDropdown.Item>
-                <NavDropdown.Item as={Link} to="/TraningPHP">PHP</NavDropdown.Item>
-                <NavDropdown.Item as={Link} to="/TraningMySql">MySQL</NavDropdown.Item>
-                <NavDropdown.Item as={Link} to="/TraningBootstrap">HTML/CSS/Bootstrap</NavDropdown.Item>
-               
-                <NavDropdown.Item as={Link} to="/TraningWebDesign">Web Development</NavDropdown.Item>
-                <NavDropdown.Item as={Link} to="/UIUXTraning">UI/UX Designer</NavDropdown.Item>
+                <NavDropdown.Item as={Link} to="/TraningRaact">
+                  React JS
+                </NavDropdown.Item>
+                <NavDropdown.Item as={Link} to="/TraningPython">
+                  Python
+                </NavDropdown.Item>
+                <NavDropdown.Item as={Link} to="/TraningPHP">
+                  PHP
+                </NavDropdown.Item>
+                <NavDropdown.Item as={Link} to="/TraningMySql">
+                  MySQL
+                </NavDropdown.Item>
+                <NavDropdown.Item as={Link} to="/TraningBootstrap">
+                  HTML/CSS/Bootstrap
+                </NavDropdown.Item>
+
+                <NavDropdown.Item as={Link} to="/TraningWebDesign">
+                  Web Development
+                </NavDropdown.Item>
+                <NavDropdown.Item as={Link} to="/UIUXTraning">
+                  UI/UX Designer
+                </NavDropdown.Item>
               </NavDropdown>
 
               <NavDropdown title="Growing Class" id="growing-class-dropdown">
-                <NavDropdown.Item as={Link} to="/TraningPython">Communication Skills</NavDropdown.Item>
-                <NavDropdown.Item as={Link} to="/TraningPython">Self-Confidence & Power Dressing</NavDropdown.Item>
-                <NavDropdown.Item as={Link} to="/TraningPython">Interview Skills</NavDropdown.Item>
-                <NavDropdown.Item as={Link} to="/TraningPython">Public Speaking</NavDropdown.Item>
+                <NavDropdown.Item as={Link} to="/TraningPython">
+                  Communication Skills
+                </NavDropdown.Item>
+                <NavDropdown.Item as={Link} to="/TraningPython">
+                  Self-Confidence & Power Dressing
+                </NavDropdown.Item>
+                <NavDropdown.Item as={Link} to="/TraningPython">
+                  Interview Skills
+                </NavDropdown.Item>
+                <NavDropdown.Item as={Link} to="/TraningPython">
+                  Public Speaking
+                </NavDropdown.Item>
               </NavDropdown>
 
               <NavDropdown title="Study Material" id="study-material-dropdown">
-                <NavDropdown.Item as={Link} to="/TraningPython">PDF Tutorial</NavDropdown.Item>
-                <NavDropdown.Item as={Link} to="/TraningPython">Video Tutorial</NavDropdown.Item>
-                <NavDropdown.Item as={Link} to="/TraningPython">Live Class</NavDropdown.Item>
+                <NavDropdown.Item as={Link} to="/TraningPython">
+                  PDF Tutorial
+                </NavDropdown.Item>
+                <NavDropdown.Item as={Link} to="/TraningPython">
+                  Video Tutorial
+                </NavDropdown.Item>
+                <NavDropdown.Item as={Link} to="/TraningPython">
+                  Live Class
+                </NavDropdown.Item>
               </NavDropdown>
 
               <NavDropdown title="Event" id="event-dropdown">
@@ -62,27 +91,47 @@ function NavBar() {
               </NavDropdown>
 
               <NavDropdown title="For Employers" id="employer-dropdown">
-                <NavDropdown.Item as={Link} to="/AdminLogin">Employee Login</NavDropdown.Item>
-                <NavDropdown.Item as={Link} to="/AdminRegistration">Employee Registration</NavDropdown.Item>
+                <NavDropdown.Item as={Link} to="/AdminLogin">
+                  Employee Login
+                </NavDropdown.Item>
+                <NavDropdown.Item as={Link} to="/AdminRegistration">
+                  Employee Registration
+                </NavDropdown.Item>
               </NavDropdown>
 
               {!isLoggedIn && (
                 <>
-                  <Nav.Link as={Link} to="/UserLogin" className="btn btn-primary custom-nav-btn-login rounded-pill px-4">
-                 User Login
+                  <Nav.Link
+                    as={Link}
+                    to="/UserLogin"
+                    className="btn btn-primary custom-nav-btn-login rounded-pill px-4"
+                  >
+                    User Login
                   </Nav.Link>
-                  <Nav.Link as={Link} to="/UserRegistration" className="btn btn-outline-primary rounded-pill custom-nav-btn px-4">
-                   User Register
+                  <Nav.Link
+                    as={Link}
+                    to="/UserRegistration"
+                    className="btn btn-outline-primary rounded-pill custom-nav-btn px-4"
+                  >
+                    User Register
                   </Nav.Link>
                 </>
               )}
 
               {isLoggedIn && (
                 <NavDropdown title="UserProfile" id="user-profile-dropdown">
-                  <NavDropdown.Item as={Link} to="/SavedJobsList">Saved Job</NavDropdown.Item>
-                  <NavDropdown.Item as={Link} to="/UserProfile">View Profile</NavDropdown.Item>
-                  <NavDropdown.Item as={Link} to="/LeftNav">JobPortal</NavDropdown.Item>
-                  <NavDropdown.Item onClick={handleLogout}>Logout</NavDropdown.Item>
+                  <NavDropdown.Item as={Link} to="/SavedJobsList">
+                    Saved Job
+                  </NavDropdown.Item>
+                  <NavDropdown.Item as={Link} to="/UserProfile">
+                    View Profile
+                  </NavDropdown.Item>
+                  <NavDropdown.Item as={Link} to="/LeftNav">
+                    JobPortal
+                  </NavDropdown.Item>
+                  <NavDropdown.Item onClick={handleLogout}>
+                    Logout
+                  </NavDropdown.Item>
                 </NavDropdown>
               )}
             </Nav>
